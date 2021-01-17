578fffa1ec51fbbe248176d2e1a499536eaeecdb
pietervanheijningen <pietervanheijningen@outlook.com>
2021-01-17 22:42:09 +0100

    Add logo and make it work better

--- js/youtube.js
+++ js/youtube.js
@@ -1,21 +1,25 @@
 function loadObserver() {
+    console.log("loading observer");
     let element = document.getElementsByClassName('ytp-upnext')[0];
 
-    let observer = new MutationObserver(function (mutationsList) {
-        for (let mutationRecord of mutationsList) {
-            if (mutationRecord.attributeName === "style" && mutationRecord.target.className === "ytp-upnext ytp-player-content ytp-suggestion-set") {
-                mutationRecord.target.querySelector("a.ytp-upnext-autoplay-icon").click();
+    if (element !== undefined) {
+        let observer = new MutationObserver(function (mutationsList) {
+            for (let mutationRecord of mutationsList) {
+                if (mutationRecord.attributeName === "style" && mutationRecord.target.className === "ytp-upnext ytp-player-content ytp-suggestion-set") {
+                    mutationRecord.target.querySelector("a.ytp-upnext-autoplay-icon").click();
+                }
             }
-        }
-    });
+        });
 
-    observer.observe(element, {attributes: true, subtree: true});
+        console.log(element);
+        observer.observe(element, {attributes: true, subtree: true});
+    }
 }
 
-loadObserver();
+setTimeout(loadObserver, 5000);
 
 chrome.runtime.onMessage.addListener(function (msg) {
     if (msg === "reloadObserver") {
-        setTimeout(loadObserver, 1000)
+        setTimeout(loadObserver, 5000)
     }
 });
